23419002c483e9cd305719f56c4709f945da4ada
Arcticmarmot <arctic_marmot@outlook.com>
2020-10-27 17:05:48 +0800

    update cdk

--- src/app/tools/scale/scale.module.ts
+++ src/app/tools/scale/scale.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScaleComponent } from './scale.component';
 import {ScaleRoutingModule} from './scale-routing.module';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
   declarations: [ScaleComponent],
   imports: [
     CommonModule,
-    ScaleRoutingModule
+    MatInputModule,
+    ScaleRoutingModule,
   ]
 })
 export class ScaleModule { }
