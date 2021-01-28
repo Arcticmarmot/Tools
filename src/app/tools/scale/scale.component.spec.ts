5b75b28a9ea7a8bf253e516dc2f68c78249da621
Arcticmarmot <arctic_marmot@outlook.com>
2021-01-28 09:21:12 +0800

    ng update

--- src/app/tools/scale/scale.component.spec.ts
+++ src/app/tools/scale/scale.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ScaleComponent } from './scale.component';
 
@@ -6,7 +6,7 @@ describe('ScaleComponent', () => {
   let component: ScaleComponent;
   let fixture: ComponentFixture<ScaleComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ScaleComponent ]
     })
